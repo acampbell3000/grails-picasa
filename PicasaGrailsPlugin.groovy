f1706cf39c79413b71c7289262e1489b54aa489d
acampbell3000 <acampbell3000@832c1c66-4827-0410-8465-ccb17913cd09>
2010-04-11 13:27:47 +0000

    Grails upgrade to 1.2.2. Introduction of the Comment domain class.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-picasa/trunk@60895 832c1c66-4827-0410-8465-ccb17913cd09

--- PicasaGrailsPlugin.groovy
+++ PicasaGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class PicasaGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.1 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [ acegi: "0.5.2" ]
+    def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
---
+++ grails-app/domain/uk/co/anthonycampbell/grails/plugins/picasa/Comment.groovy
@@ -0,0 +1,7 @@
+package uk.co.anthonycampbell.grails.plugins.picasa
+
+class Comment {
+
+    static constraints = {
+    }
+}
