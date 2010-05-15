46aa848e0804d10e97fd3db89e47691667550ed7
acampbell3000 <acampbell3000@832c1c66-4827-0410-8465-ccb17913cd09>
2010-05-15 10:32:25 +0000

    Trunk clean up. Version number update.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-picasa/trunk@61563 832c1c66-4827-0410-8465-ccb17913cd09

--- PicasaGrailsPlugin.groovy
+++ PicasaGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class PicasaGrailsPlugin {
     // the plugin version
-    def version = "0.4.1"
+    def version = "0.4.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.1 > *"
     // the other plugins this plugin depends on
