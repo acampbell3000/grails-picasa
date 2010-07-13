0f3eecf951c5238347d818da5976b4012fcbf5f1
acampbell3000 <acampbell3000@832c1c66-4827-0410-8465-ccb17913cd09>
2010-07-13 22:13:53 +0000

    CHANGE log update. Small fix to photo comment listing. Introduction to the PicasaCommentService.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-picasa/trunk@62892 832c1c66-4827-0410-8465-ccb17913cd09

---
+++ test/unit/uk/co/anthonycampbell/grails/plugins/picasa/PicasaCommentServiceServiceTests.groovy
@@ -0,0 +1,17 @@
+package uk.co.anthonycampbell.grails.plugins.picasa
+
+import grails.test.*
+
+class PicasaCommentServiceServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
